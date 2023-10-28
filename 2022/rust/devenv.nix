f84a5daeae3d655085997972a0687616908d2eb5
Jordan Garrison <jordangarrison@users.noreply.github.com>
2023-10-28 08:38:26 +0000

    testing devenv out with rust, it has been notorious for being hard to set up on nixos, this seems to make it really easy

---
+++ 2022/rust/devenv.nix
@@ -0,0 +1,38 @@
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = with pkgs; [
+    git
+    github-cli
+
+  ];
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = "echo hello from $GREET";
+
+  enterShell = ''
+    hello
+    git --version
+  '';
+
+  # https://devenv.sh/languages/
+  languages = {
+    nix.enable = true;
+    rust = {
+      enable = true;
+      channel = "stable";
+    };
+  };
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+
+  # See full reference at https://devenv.sh/reference/options/
+}
